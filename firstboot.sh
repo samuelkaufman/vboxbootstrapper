3d5ccf4787fe03b8f6fd36cf5d680b9e4b3f3e63
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2018-07-13 19:37:22 +0000

    google app engine

--- firstboot.sh
+++ firstboot.sh
@@ -117,12 +117,6 @@ cp /etc/skel/.profile /home/$MYUSER/.profile
 ##Set up google utils gsutil
 # Create an environment variable for the correct distribution
 
-function install_google_cloud_sdk() {
-  if test -e /etc/apt/sources.list.d/google-cloud-sdk.list;
-  then
-    echo "google cloud sdk already installed."
-    return
-  fi
 # Add the Cloud SDK distribution URI as a package source
 apt-get install --no-install-recommends -y lsb-release
 echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -132,11 +126,8 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # Update and install the Cloud SDK
 apt-get update
-apt-get install -y --no-install-recommends google-cloud-sdk
+apt-get install -y --no-install-recommends google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-go
 
-}
-
-install_google_cloud_sdk
 
 ##/End gsutil
 
